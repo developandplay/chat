6385651821014cdfe2bada269061e397c77ba30c
developandplay <martin.troiber@gmail.com>
2018-10-21 00:45:31 +0200

    added auto update feature

---
+++ api/update.php
@@ -0,0 +1,15 @@
+<?
+file_put_contents("master.zip", 
+    file_get_contents("https://github.com/developandplay/chat/archive/test.zip")
+);
+$zip = new ZipArchive;
+$res = $zip->open("master.zip");
+if ($res === TRUE) {
+  $zip->extractTo(dirname( dirname(__FILE__) ));
+  $zip->close();
+  unlink("master.zip");
+  echo 'Update successfull!';
+} else {
+  echo 'Update failded!';
+}
+?>
