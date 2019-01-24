83501406f1f9229b53d64c80819784d9811f47db
developandplay <34752929+developandplay@users.noreply.github.com>
2019-01-24 23:05:54 +0100

    changed directory levels

--- api/update.php
+++ api/update.php
@@ -5,7 +5,7 @@ file_put_contents("master.zip",
 $zip = new ZipArchive;
 $res = $zip->open("master.zip");
 if ($res === TRUE) {
-  $zip->extractTo(dirname( dirname(__FILE__) ));
+  $zip->extractTo(dirname(__FILE__),2);
   $zip->close();
   unlink("master.zip");
   echo 'Update successfull!';
