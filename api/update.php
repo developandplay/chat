37de026e271d3e74ed00f8c71ef5021d6439228f
developandplay <34752929+developandplay@users.noreply.github.com>
2019-01-24 23:16:40 +0100

    remove old files beforehand

--- api/update.php
+++ api/update.php
@@ -1,11 +1,17 @@
 <?
+$path=dirname(dirname(dirname(__FILE__)));
 file_put_contents("master.zip", 
     file_get_contents("https://github.com/developandplay/chat/archive/master.zip")
 );
 $zip = new ZipArchive;
 $res = $zip->open("master.zip");
 if ($res === TRUE) {
-  $zip->extractTo(dirname(dirname(dirname(__FILE__))));
+  $files = glob($path); // get all file names
+  foreach($files as $file){ // iterate files
+    if(is_file($file))
+      unlink($file); // delete file
+  }
+  $zip->extractTo($path);
   $zip->close();
   unlink("master.zip");
   echo 'Update successfull!';
