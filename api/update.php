9345e5e88a7439a1d6cd30a5e649ac8c1a5d9cc8
developandplay <34752929+developandplay@users.noreply.github.com>
2019-01-24 23:09:12 +0100

    new directory setting

--- api/update.php
+++ api/update.php
@@ -5,7 +5,7 @@ file_put_contents("master.zip",
 $zip = new ZipArchive;
 $res = $zip->open("master.zip");
 if ($res === TRUE) {
-  $zip->extractTo(dirname(__FILE__),2);
+  $zip->extractTo(dirname(dirname(dirname(__FILE__))));
   $zip->close();
   unlink("master.zip");
   echo 'Update successfull!';
